35e6b09d2c1569da66e65e71b684084f2c192fcf
Aya <hdiyan122@gmail.com>
2026-01-14 19:49:19 +0100

    Add configuration file for bot settings

---
+++ config.js
@@ -0,0 +1,19 @@
+// config.js
+const config = {
+    botName: "AyaTech",
+    ownerName: "Aya Es-samlaly",
+    brand: "digitera.io",
+    ownerNumber: ["212728254498"], // Add your number with country code
+    prefix: ".", 
+    sessionName: "session",
+    footer: "© 2026 digitera.io | Morocco 🇲🇦",
+    
+    // Social & Contact
+    links: {
+        instagram: "https://www.instagram.com/digitera.io",
+        whatsappChannel: "https://whatsapp.com/channel/0029VbC47zXBlHpdQcVriR1Y",
+        email: "ayaes-samlalytech@proton.me"
+    }
+};
+
+module.exports = config;
